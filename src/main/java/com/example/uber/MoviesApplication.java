957803b3e84adb813f29c022825614c102cd53a2
kopz9 <gustavoeduardo438@gmail.com>
2025-05-06 13:33:03 -0300

    name refactor

--- src/main/java/com/example/uber/MoviesApplication.java
+++ src/main/java/com/example/uber/MoviesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UberApplication {
+public class MoviesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UberApplication.class, args);
+		SpringApplication.run(MoviesApplication.class, args);
 	}
 
 }
